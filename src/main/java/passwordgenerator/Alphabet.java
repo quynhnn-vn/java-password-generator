9a58b5af55a785b5236b8ba304d62f568ab85797
nnquynh.0111@gmail.com <nnquynh.0111@gmail.com>
2023-08-16 10:44:45 +0200

    Remove silly comments

--- src/main/java/passwordgenerator/Alphabet.java
+++ src/main/java/passwordgenerator/Alphabet.java
@@ -1,15 +1,11 @@
 package passwordgenerator;
 
 public class Alphabet {
-    // public: access from anywhere in the program
-    // static: belong to class rather than to instances of the class
-    // finale: can not be changed once the variable is assigned
     public static final String UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public static final String LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
     public static final String NUMBERS = "0123456789";
     public static final String SYMBOLS = "!@#$%^&*()-_=+\\\\/~?";
 
-    // private: access within the class
     private final StringBuilder stringBuilder = new StringBuilder();
 
     public Alphabet(boolean uppercaseIncluded, boolean lowercaseIncluded, boolean numbersIncluded, boolean specialCharactersIncluded) {
--- src/test/java/passwordgenerator/AppTest.java
+++ src/test/java/passwordgenerator/AppTest.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
     private final Password password= new Password("Secret");
